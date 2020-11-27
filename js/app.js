c0b57aa65cfe482651fcd6985e911f146975a891
lindsera1 <52899223+lindsera1@users.noreply.github.com>
2020-11-27 15:53:26 -0600

    updates

--- js/app.js
+++ js/app.js
@@ -15,3 +15,16 @@ data.forEach((dataRow) => {
         cell.text(val);
     });
 });
+
+function handleClick(){
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+      };
+    buildTable(filteredData);
+}
+
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+buildTable(tableData);
